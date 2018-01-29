a9c629591f7f89767673c9de8a5f8341c909e716
徐茂怀 <18040104170@163.com>
2018-01-29 12:55:11 +0800

    啊
      发

---
+++ 3D_Touch/aaaTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  aaaTableViewCell.h
+//  3D_Touch
+//
+//  Created by 徐茂怀 on 2018/1/29.
+//  Copyright © 2018年 徐茂怀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface aaaTableViewCell : UITableViewCell
+
+@end
---
+++ 3D_Touch/aaaTableViewCell.m
@@ -0,0 +1,26 @@
+//
+//  aaaTableViewCell.m
+//  3D_Touch
+//
+//  Created by 徐茂怀 on 2018/1/29.
+//  Copyright © 2018年 徐茂怀. All rights reserved.
+//
+
+#import "aaaTableViewCell.h"
+
+@implementation aaaTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+    NSLog(@"12332122");
+    
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
